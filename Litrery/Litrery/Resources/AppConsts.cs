f4338818b588c243bddce51ae485b6c6a7ea944c
Tefo Hashatse <thashatse@gmail.com>
2026-04-19 16:46:02 +0200

    Added AppConsts.cs

---
+++ Litrery/Litrery/Resources/AppConsts.cs
@@ -0,0 +1,8 @@
+namespace Litrery.Resources;
+
+public static class AppConsts
+{
+    public const string DOCUMENT_DB_ENDPOINT = "";
+    public const string DOCUMENT_DB_KEY = "";
+    public const string DOCUMENT_DB_NAME = "Litrery";
+}
